9c33b23fe60d11a1b0617dcabee51790fd567715
Philani Ncube <ncbphi001@gmail.com>
2024-05-08 12:08:19 +0200

    Update About page content layout and styling

--- components/About/Content.jsx
+++ components/About/Content.jsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 const Content = () => {
   return (
     <Fragment>
-      <div className="max-w-6xl mx-auto py-12 bg-white">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8">
+      <div className="max-w-6xl py-12 mx-auto bg-white">
+        <div className="grid grid-cols-1 py-8 md:grid-cols-2 lg:grid-cols-3">
           <div className="col-span-1 lg:col-span-2">
-            <h3 className="text-2xl md:text-3xl lg:text-4xl text-black uppercase">
+            <h3 className="text-2xl text-black uppercase md:text-3xl lg:text-4xl">
               From the beginning
             </h3>
-            <p className="text-xl text-gray-700 mt-2 lg:pr-24">
+            <p className="mt-2 text-xl text-gray-700 lg:pr-24">
               Bronscor was established in 1992 as a solely owned black company,
               supplying maintenance materials & engineering services to
               factories, manufacturing concerns & engineering companies
@@ -23,41 +23,41 @@ const Content = () => {
               alt=""
               width={960}
               height={640}
-              className="h-full object-cover"
+              className="object-cover h-full"
             />
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8">
+        <div className="grid grid-cols-1 py-8 md:grid-cols-2 lg:grid-cols-3">
           <div className="col-span-1 max-h-[400px]">
             <Image
               src="/images/truck.jpg"
               alt=""
               width={960}
               height={640}
-              className="h-full object-cover"
+              className="object-cover h-full"
             />
           </div>
-          <div className="col-span-1 lg:col-span-2 flex flex-col items-end">
-            <h3 className="text-2xl md:text-3xl lg:text-4xl text-black uppercase">
+          <div className="flex flex-col items-end col-span-1 lg:col-span-2">
+            <h3 className="text-2xl text-black uppercase md:text-3xl lg:text-4xl">
               Great Service
             </h3>
-            <p className="text-xl text-gray-700 mt-2 text-right md:pl-24">
+            <p className="mt-2 text-xl text-right text-gray-700 md:pl-24">
               We pride ourselves on service, offering cut to size on most of our
               materials and a 24 hour breakdown service.
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8 text-gray-700">
+        <div className="grid grid-cols-1 py-8 text-gray-700 md:grid-cols-2 lg:grid-cols-3">
           <div className="col-span-1 lg:col-span-2">
-            <h3 className="text-2xl md:text-3xl lg:text-4xl text-black uppercase">
+            <h3 className="text-2xl text-black uppercase md:text-3xl lg:text-4xl">
               Our Vision
             </h3>
-            <p className="text-xl text-gray-700 mt-2 lg:pr-24">
+            <p className="mt-2 text-xl text-gray-700 lg:pr-24">
               To be one of the top leading special steels & engineering service
               suppliers in Sub-Saharan Africa.
             </p>
-            <h4 className="text-lg mt-2">Our Mission</h4>
-            <ul className="text-md list-disc pl-6">
+            <h4 className="mt-2 text-lg">Our Mission</h4>
+            <ul className="pl-6 list-disc text-md">
               <li>To provide excellent service to our customers</li>
               <li>To provide a safe working environment for our staff</li>
               <li>To supply top quality products</li>
@@ -69,9 +69,27 @@ const Content = () => {
               alt=""
               width={960}
               height={640}
-              className="h-full object-cover"
+              className="object-cover h-full"
             />
           </div>
+        </div>
+        <div className="grid grid-cols-1 py-8 text-gray-700">
+          <h4 className="text-2xl text-black uppercase md:text-3xl lg:text-4xl">
+            Shipping and Delivery
+          </h4>
+          <ul className="pl-6 list-disc text-md">
+            <li>
+              Once an order has been received, we will process your order
+              internally.
+            </li>
+            <li>
+              Once the order is ready, a waybill will be sent to the customer.
+            </li>
+            <li>
+              The order will be shipped to the customer. Depending on location
+              or area, this will take between 1 to 3 days.
+            </li>
+          </ul>
         </div>
       </div>
     </Fragment>
--- pages/tools/index.jsx
+++ pages/tools/index.jsx
@@ -9,24 +9,24 @@ const index = () => {
         style={{ backgroundImage: 'url(/images/products-banner.jpg)' }}
       >
         <div className="bg-brown bg-opacity-70 lg:h-[50%] lg:w-2/5 py-4  xl:pl-[130px] translate-y-1/2 flex flex-col justify-center text-left">
-          <h1 className="text-3xl xl:text-5xl uppercase px-6 lg:px-0">Tools</h1>
+          <h1 className="px-6 text-3xl uppercase xl:text-5xl lg:px-0">Tools</h1>
         </div>
       </div>
-      <main className="max-w-6xl mx-auto py-16 bg-white">
+      <main className="max-w-6xl py-16 mx-auto bg-white">
         <h1 className="text-3xl font-bold">Our Specials</h1>
-        <div className="flex space-x-6 items-center justify-center">
+        <div className="flex items-center justify-center space-x-6">
           <Link href="/documents/Specials.pdf" passHref>
-            <button className="bg-black text-brown py-2 px-8">
+            <button type="button" className="px-8 py-2 bg-black text-brown">
               Back At Work Specials
             </button>
           </Link>
           <Link href="/documents/Q20.pdf" passHref>
-            <button className="bg-black text-brown py-2 px-8">
+            <button type="button" className="px-8 py-2 bg-black text-brown">
               Q20 Specials
             </button>
           </Link>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 py-12">
+        <div className="grid grid-cols-1 gap-12 py-12 md:grid-cols-2">
           <p className="text-sm">
             Big or small, whatever tools you need for your business, we can
             assist. We are not limited to the products displayed in the
